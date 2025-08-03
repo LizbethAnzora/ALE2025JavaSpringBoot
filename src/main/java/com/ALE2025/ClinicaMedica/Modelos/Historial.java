535d1df127e717fed4d45403a329bf123f234d00
Josue <josueadolfov4lle@gmail.com>
2025-08-03 15:30:07 -0600

    Se creo la Entidad de Historial

--- src/main/java/com/ALE2025/ClinicaMedica/Modelos/Historial.java
+++ src/main/java/com/ALE2025/ClinicaMedica/Modelos/Historial.java
@@ -1,5 +1,79 @@
 package com.ALE2025.ClinicaMedica.Modelos;
 
-public class Historial {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import java.util.Date;
 
+@Entity
+@Table(name = "Historiales")
+public class Historial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "paciente_id", referencedColumnName = "id")
+    private Paciente paciente;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "medico_id", referencedColumnName = "id")
+    private Medico medico;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "especialidad_id", referencedColumnName = "id")
+    private Especialidad especialidad;
+
+    @NotNull(message = "La fecha de la cita es requerida")
+    @Temporal(TemporalType.DATE)
+    private Date fechaCita;
+
+    private String observaciones;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public Especialidad getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(Especialidad especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public Date getFechaCita() {
+        return fechaCita;
+    }
+
+    public void setFechaCita(Date fechaCita) {
+        this.fechaCita = fechaCita;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
 }
