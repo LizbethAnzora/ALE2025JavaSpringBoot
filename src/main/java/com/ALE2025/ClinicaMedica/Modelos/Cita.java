17ebe97486e32c15b78823cb1b74ee67d80bd5aa
EsmeSnchz08052006 <ms25001@esfe.agape.edu.sv>
2025-08-03 20:52:33 -0600

    Se crearo las entidades de cita y horario

--- src/main/java/com/ALE2025/ClinicaMedica/Modelos/Cita.java
+++ src/main/java/com/ALE2025/ClinicaMedica/Modelos/Cita.java
@@ -1,5 +1,108 @@
 package com.ALE2025.ClinicaMedica.Modelos;
 
-public class Cita {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.sql.Time;
+import java.util.Date;
+import java.util.List;
 
+@Entity
+@Table(name = "Citas")
+public class Cita {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "paciente_id", referencedColumnName = "id")
+    private Paciente paciente;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "medico_id", referencedColumnName = "id")
+    private Medico medico;
+
+    @NotNull(message = "La fecha de la cita es requerida")
+    @Temporal(TemporalType.DATE)
+    private Date fechaCita;
+
+    @NotNull(message = "La hora de la cita es requerida")
+    private Time horaCita;
+
+    @NotNull(message = "El costo de la consulta es requerido")
+    private BigDecimal costoConsulta;
+
+    @Enumerated(EnumType.STRING)
+    private EstadoCita estado;
+
+    @OneToMany(mappedBy = "cita", cascade = CascadeType.ALL)
+    private List<Historial> historiales;
+
+    public enum EstadoCita {
+        Pendiente, Confirmada, Cancelada, Finalizada
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public Date getFechaCita() {
+        return fechaCita;
+    }
+
+    public void setFechaCita(Date fechaCita) {
+        this.fechaCita = fechaCita;
+    }
+
+    public Time getHoraCita() {
+        return horaCita;
+    }
+
+    public void setHoraCita(Time horaCita) {
+        this.horaCita = horaCita;
+    }
+
+    public BigDecimal getCostoConsulta() {
+        return costoConsulta;
+    }
+
+    public void setCostoConsulta(BigDecimal costoConsulta) {
+        this.costoConsulta = costoConsulta;
+    }
+
+    public EstadoCita getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoCita estado) {
+        this.estado = estado;
+    }
+
+    public List<Historial> getHistoriales() {
+        return historiales;
+    }
+
+    public void setHistoriales(List<Historial> historiales) {
+        this.historiales = historiales;
+    }
 }
--- src/main/java/com/ALE2025/ClinicaMedica/Modelos/Horario.java
+++ src/main/java/com/ALE2025/ClinicaMedica/Modelos/Horario.java
@@ -1,5 +1,72 @@
 package com.ALE2025.ClinicaMedica.Modelos;
 
-public class Horario {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import java.sql.Time;
 
+
+@Entity
+@Table(name = "Horarios")
+public class Horario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "medico_id", referencedColumnName = "id")
+    private Medico medico;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "El día de la semana es requerido")
+    private DiaSemana diaSemana;
+
+    @NotNull(message = "La hora de inicio es requerida")
+    private Time horaInicio;
+
+    @NotNull(message = "La hora de fin es requerida")
+    private Time horaFin;
+
+    public enum DiaSemana {
+        Lunes, Martes, Miércoles, Jueves, Viernes, Sábado, Domingo
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public DiaSemana getDiaSemana() {
+        return diaSemana;
+    }
+
+    public void setDiaSemana(DiaSemana diaSemana) {
+        this.diaSemana = diaSemana;
+    }
+
+    public Time getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(Time horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public Time getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(Time horaFin) {
+        this.horaFin = horaFin;
+    }
 }
